148ae47b38978b929cfa7735c255bc30d94e4ff3
Hiagor Vieira <hiagor.vieira@inovafarma.com.br>
2025-12-08 17:32:19 -0300

    improve: adds the entities

---
+++ S-SingleResponsability/After/Entities/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SOLID_By_Example.S_SingleResponsability.After.Entities
+{
+    public class Item
+    {
+        public Item(int amount, decimal price)
+        {
+            if (amount <= 0)
+                throw new Exception("The item quantity cannot be equal to or less than zero.");
+
+            if (price <= 0)
+                throw new Exception("The item price cannot be less than or equal to zero.");
+
+            Amount = amount;
+            Price = price;
+        }
+
+        public int Amount { get; private set; }
+        public decimal Price { get; private set; }
+    }
+}
---
+++ S-SingleResponsability/After/Entities/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SOLID_By_Example.S_SingleResponsability.After.Entities
+{
+    public class Order 
+    {
+        public Order(int id, string customer, List<Item> items)
+        {
+            if (string.IsNullOrWhiteSpace(customer))
+                throw new Exception("Invalid customer.");
+
+            if (items == null || items.Count == 0)
+                throw new Exception("Order without items.");
+
+            IdOrder = id;
+            Customer = customer;
+            Items = items;
+            Total = CalculateTotal();
+        }
+
+        public int IdOrder { get; private set; }
+        public string Customer { get; private set; }
+        public List<Item> Items { get; private set; }
+        public decimal Total { get; private set; }
+
+        private decimal CalculateTotal()
+        {
+            return Items.Sum(item => item.Price * item.Amount);
+        }
+    }
+}
